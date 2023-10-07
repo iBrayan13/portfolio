afd8a548aefe7e72cb9127186065aa49281d21b5
brayanbarret0 <brayanbarreto1130@gmail.com>
2023-10-06 22:45:54 -0400

    Getting custom design

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="navbar">
+        <ul>
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>Resume</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+  
+<style lang="sass">
+.navbar
+    @include reset-pm()
+    background-color: $background-second-color
+    border: 0
+    border-radius: $radius-avg
+    @include delete-border-radius($bottom-right: 'true', $bottom-left: 'true')
+    display: flex
+    justify-content: center
+    ul
+        @include set-size($width: 40%, $height: calc(3rem - 1rem))
+        background-color: $background-third-color
+        border-radius: $radius-avg
+        padding: 0.5rem
+        margin-top: 1.5rem
+        margin-bottom: 0.5rem
+        list-style: none
+        display: flex
+        flex-direction: row
+        align-items: center
+        justify-content: center
+        gap: $radius-avg
+
+        li
+            transition: all ease 0.25s
+
+            &:hover
+                cursor: pointer
+                color: $background-second-color
+        
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContentComponent />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ContentComponent from '@/components/ContentComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ContentComponent
   }
 }
 </script>
+
+<style lang="sass">
+.home
+  display: flex
+  justify-content: center
+</style>
